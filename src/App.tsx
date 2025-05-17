9af71330ce6ab96ffbc15f22735aa894bc3e12fc
稲川貴大 <ina111@inagawatakashidainoMac-mini.local>
2025-05-17 23:28:07 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react'
+import Globe from 'react-globe.gl'
+import type { GlobeMethods } from 'react-globe.gl'
+
+function App() {
+  const globeEl = useRef<GlobeMethods | undefined>(undefined)
+
+  useEffect(() => {
+    globeEl.current?.pointOfView({ lat: 0, lng: 0, altitude: 2 }, 0)
+  }, [])
+
+  return (
+    <div style={{ position: 'fixed', width: '100%', height: '100%' }}>
+      <Globe
+        ref={globeEl}
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+      />
+    </div>
+  )
+}
+
+export default App
