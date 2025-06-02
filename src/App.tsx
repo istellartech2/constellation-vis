145a8c5345a1bdf1f97a69249cf1bb5f95621512
ina111 <ina111meister+github@gmail.com>
2025-06-02 10:14:58 +0900

    Add ECEF view option

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,7 @@ function App() {
   const [showGraticule, setShowGraticule] = useState(true);
   const [showEcliptic, setShowEcliptic] = useState(true);
   const [showSunDirection, setShowSunDirection] = useState(true);
+  const [ecef, setEcef] = useState(false);
 
   const [startTime, setStartTime] = useState(() => {
     const d = new Date();
@@ -63,6 +64,7 @@ function App() {
     showGraticule,
     showEcliptic,
     showSunDirection,
+    ecef,
     onSelect: setSelectedIdx,
     onSelectStation: setSelectedGsIdx,
     stationInfoRef: gsInfoRef,
@@ -138,6 +140,8 @@ function App() {
         onShowEclipticChange={setShowEcliptic}
         showSunDirection={showSunDirection}
         onShowSunDirectionChange={setShowSunDirection}
+        ecef={ecef}
+        onEcefChange={setEcef}
         onUpdate={(s, gs, start) => {
           setSatellites(s);
           setGroundStations(gs);
